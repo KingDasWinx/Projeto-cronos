784adee66aea19f7666540b59a0f3219e77527a8
KingDasWinx <jvnqsoumheroiking@gmail.com>
2023-08-11 22:10:28 -0300

    primeiro commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,8 @@
+
+function Home() {
+    return(
+        <h1>Motta é gordo</h1>
+    )
+}
+
+export default Home;
---
+++ src/routes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+const Rotas = () => {
+  return(
+    <Routes>
+        
+    </Routes>
+  )
+};
+
+export default Rotas;
