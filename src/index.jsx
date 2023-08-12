36fc4bf114e6c255afd4666e5a249eac45bf7a11
KingDasWinx <jvnqsoumheroiking@gmail.com>
2023-08-11 22:13:03 -0300

    teste

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import Home from './pages/home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
     <Home/>
   </React.StrictMode>
 );
